17d6cfe27aa440e7b4dd86c49949aed58969d347
mzw86020424 <mzw86020424@gmail.com>
2021-02-18 17:33:22 +0900

    learn class

--- myapp.py
+++ myapp.py
@@ -1,6 +1,14 @@
-def say_hi():
+class User:
     pass
 
 
-msg = say_hi()
-print(msg)
+tom = User()
+tom.name = "tom"
+tom.score = 20
+
+bob = User()
+bob.name = "bob"
+bob.level = 5
+
+print(tom.name)
+print(bob.level)
