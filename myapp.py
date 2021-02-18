c09bf7403d00abb549d57e802a141afbaaaf1850
mzw86020424 <mzw86020424@gmail.com>
2021-02-18 17:35:56 +0900

    learn about constructor

--- myapp.py
+++ myapp.py
@@ -1,14 +1,24 @@
+# class User:
+#     pass
+
+# tom = User()
+# tom.name = "tom"
+# tom.score = 20
+
+# bob = User()
+# bob.name = "bob"
+# bob.level = 5
+
+# print(tom.name)
+# print(bob.level)
+
 class User:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
-tom = User()
-tom.name = "tom"
-tom.score = 20
-
-bob = User()
-bob.name = "bob"
-bob.level = 5
+tom = User("tom")
+bob = User("bob")
 
 print(tom.name)
-print(bob.level)
+print(bob.name)
