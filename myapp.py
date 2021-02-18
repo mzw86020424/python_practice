6b38115b16b98986648252ac5d78ccda117a1224
mzw86020424 <mzw86020424@gmail.com>
2021-02-18 20:59:12 +0900

    use filter

--- myapp.py
+++ myapp.py
@@ -1,11 +1,7 @@
-# map(関数, イテレータ)
+# filter(関数, イテレータ)
+def is_even(n):
+    return n % 3 == 0
 
-# def triple(n):
-#     return n * 3
 
-
-# print(list(map(triple, [1, 2, 3])))
-
-# lambda 引数: 処理
-
-print(list(map(lambda n: n*3, [1, 2, 3])))
+# print(list(filter(is_even, range(10))))
+print(list(filter(lambda n: n % 2 == 0, range(10))))
