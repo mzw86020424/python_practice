9bb2d5a608412557e8d00ab176319ae107e8c9d9
mzw86020424 <mzw86020424@gmail.com>
2021-02-18 20:28:38 +0900

    create tuple

--- myapp.py
+++ myapp.py
@@ -1,10 +1,3 @@
-scores = [40, 50]
-print(scores[0])
-scores[0] = 45
-print(len(scores))
-scores.append(100)
-for score in scores:
-    print(score)
-
-for i, score in enumerate(scores):
-    print('{0} : {1}'.format(i, score))
+items = (50, "apple", 23.5)
+print(items[1])
+items[1] = 'pen'
