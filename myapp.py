94eaf747a86e39e0eaae03b9750271da0386fe29
mzw86020424 <mzw86020424@gmail.com>
2021-02-18 17:54:56 +0900

    extend AdminUser from User

--- myapp.py
+++ myapp.py
@@ -1,23 +1,24 @@
 class User:
-    count = 0
-
     def __init__(self, name):
-        User.count += 1
         self.name = name
 
     def say_hi(self):
         print("hi {0}".format(self.name))
 
-    @classmethod
-    def show_info(cls):
-        print("{0} instance".format(cls.count))
+
+class AdminUser(User):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+    def say_hello(self):
+        print("hello {0} ({1})".format(self.name, self.age))
+
+    def say_hi(self):
+        print("[admin] hi {0}".format(self.name))
 
 
-print(User.count)
-tom = User("tom")
-bob = User("bob")
+bob = AdminUser("bob", 23)
 
-tom.say_hi()
+bob.say_hello()
 bob.say_hi()
-
-User.show_info()
