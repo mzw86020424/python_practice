719dcc564e590ba6213d1f299518148813731491
mzw86020424 <mzw86020424@gmail.com>
2021-02-18 18:11:50 +0900

    create package

--- myapp.py
+++ myapp.py
@@ -1,6 +1,10 @@
-from user import User, AdminUser
+# from user import User, AdminUser
+# from mypackage.user import User, AdminUser
+import mypackage.user
 
-bob = AdminUser("bob", 23)
 
+bob = mypackage.user.AdminUser("bob", 23)
+
+print(bob.name)
 bob.say_hello()
 bob.say_hi()
