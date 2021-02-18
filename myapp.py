ecb97beb73b60ac86d479d169d0b1285ad2002d8
mzw86020424 <mzw86020424@gmail.com>
2021-02-18 18:02:57 +0900

    make module

--- myapp.py
+++ myapp.py
@@ -1,22 +1,4 @@
-class User:
-    def __init__(self, name):
-        self.name = name
-
-    def say_hi(self):
-        print("hi {0}".format(self.name))
-
-
-class AdminUser(User):
-    def __init__(self, name, age):
-        super().__init__(name)
-        self.age = age
-
-    def say_hello(self):
-        print("hello {0} ({1})".format(self.name, self.age))
-
-    def say_hi(self):
-        print("[admin] hi {0}".format(self.name))
-
+from user import User, AdminUser
 
 bob = AdminUser("bob", 23)
 
